9ca25a089dc5aa7539c5000d50199ff277cd75f5
Mike Funk <mike@mikefunk.com>
2013-08-11 18:45:25 -0400

    test env set up

---
+++ tests/MikeFunk/QueryStringer/Tests/QueryStringerTest.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package QueryStringer
+ * @copyright 2013 Xulon Press, Inc. All Rights Reserved.
+ */
+namespace MikeFunk\Tests;
+
+use Mockery;
+
+/**
+ * QueryStringerTest
+ *
+ * @author Michael Funk <mike@mikefunk.com>
+ */
+class QueryStringerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * tearDown
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+    /**
+     * testGet
+     *
+     * @return void
+     */
+    public function testGet()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * testWith
+     *
+     * @return void
+     */
+    public function testWith()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * testWithout
+     *
+     * @return void
+     */
+    public function testWithout()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * testGetArray
+     *
+     * @return void
+     */
+    public function testGetArray()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * replaceWith
+     *
+     * @return void
+     */
+    public function replaceWith()
+    {
+        $this->markTestIncomplete();
+    }
+}
